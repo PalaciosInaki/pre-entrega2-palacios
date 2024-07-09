176296656c06644f87b5233dcf2045af8e2a9029
Inaki Palacios <palaciosinia@gmail.com>
2024-07-09 20:52:07 -0300

    .

--- main.js
+++ main.js
@@ -1,82 +1,67 @@
-/* Simulador carrito de compras */
+const carrito = [];
 
-function agregarAlCarrito(precio) {
-    totalCompra += precio; /* Sumo al valor total de la compra el precio indicado por parametro */
-    alert('Producto agregado al carrito.\n' +
-        'Total actual: $' + totalCompra);
-}
+const mostrarProductos = () => {
+    let listaProductos = "Productos Disponibles:\n";
+    productos.forEach(producto => {
+        listaProductos += `${producto.id}: ${producto.nombre} - $${producto.precio}\n`;
+    });
+    alert(listaProductos);
+};
 
-function mostrarProductos() {
+const agregarAlCarrito = (idProducto, cantidad) => {
+    // Busco producto por id
+    const productoEncontrado = productos.find(producto => producto.id === idProducto);
 
-    let producto = prompt('Productos disponibles:\n' +
-        '1. Grafica RTX 4080 - $500\n' +
-        '2. Pc Gamer Prearmada - $1000\n' +
-        '3. Combo Perifericos Hyper X - $250' + '\n\n' +
-        'Ingresa el número del producto que deseas agregar al carrito:');
-
-    switch (producto) {
-        case '1':
-            agregarAlCarrito(500); /* Llamo a la funcion agregarAlCarrito y le paso como parametro el precio del producto */
-            break;
-        case '2':
-            agregarAlCarrito(1000); /* Llamo a la funcion agregarAlCarrito y le paso como parametro el precio del producto */
-            break;
-        case '3':
-            agregarAlCarrito(250); /* Llamo a la funcion agregarAlCarrito y le paso como parametro el precio del producto */
-            break;
-        default:
-            alert('Producto no válido. Por favor selecciona un producto válido.');
-            mostrarProductos(); /* Vuelvo a mostrar los productos */
-            break;
+    if (productoEncontrado && cantidad > 0) {
+        carrito.push({
+            id: productoEncontrado.id,
+            nombre: productoEncontrado.nombre,
+            precio: productoEncontrado.precio,
+            cantidad: cantidad
+        });
+        if(cantidad > 1){
+            console.log(`Se añadio ${cantidad} unidades de ${productoEncontrado.nombre} al carrito.`);
+        }else{
+            console.log(`Se añadio ${cantidad} unidad de ${productoEncontrado.nombre} al carrito.`);
+        }
+    } else {
+        console.log("El producto no existe o cantidad invalida.");
     }
-}
+};
 
-function finalizarCompra() {
-    alert('Compra finalizada.\n\n' +
-        'Total a pagar: $' + totalCompra);
-    totalCompra = 0; /* Reinicio el valor del totalCompra para la sig */
-}
 
-function terminarSimulacion() {
-    alert('¡Gracias por tu compra y por elegirnos!\n' +
-        'Somos Bamboo PC'
-    );
-}
+const calcularTotal = () => {
+    let total = 0;
 
-/* Inicializo variables */
-let opcion = 0;
-let totalCompra = 0;
+    carrito.forEach(producto => {
+        total += producto.precio * producto.cantidad;
+    });
 
-/* Inicio la simulación  */
-while (opcion != 3) {
+    return total;
+};
 
-    opcion = prompt('Bienvenido al carrito de compras de Bamboo PC.\n' +
-        'Selecciona una opción:\n' +
-        '1. Ver productos disponibles\n' +
-        '2. Finalizar compra\n' +
-        '3. Salir');
 
-    switch (opcion) {
-        case '1':
-            mostrarProductos();
-            break;
-        case '2':
-            finalizarCompra();
-            break;
-        case '3':
-            terminarSimulacion();
-            break;
-        default:
-            alert('Opción no válida. Por favor selecciona una opción válida.\n' +
-                opcion);
-            break;
+const seleccionProductos = () => {
+
+    let continuar = true;
+
+    while (continuar) {
+        mostrarProductos();
+
+        //Entrada del usuario
+        const idProducto = parseInt(prompt("Ingrese el ID del producto que desea agregar al carrito:"));
+        const cantidad = parseInt(prompt("Ingrese la cantidad de unidades:"));
+
+        agregarAlCarrito(idProducto, cantidad);
+
+        continuar = confirm ("Desea agregar mas productos al carrito?");
     }
 
-}
+    alert(`Total de su compra: $${calcularTotal()}`);
+
+};
 
 
-
-
-
+seleccionProductos();
 
 
